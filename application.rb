15edc7c273f5b416c5487a5930437c90b00b9856
Ganesh Subramanian <gsubu@imac-21.zoomtown.com>
2013-02-28 07:31:45 -0500

    draft10

--- application.rb
+++ application.rb
@@ -13,6 +13,28 @@ get '/idropcopy' do
   haml :idropcopy
 end
 
+get '/idropcopy/help/:platform' do
+  @link = '/idropcopy/help'
+  @platform = params[:platform]
+  haml :'idropcopy/help/help', :locals => {:link => @link, :platform => @platform}
+end
+
+get '/idropcopy/p/help' do
+  @link = '/idropcopy/p/help'
+  haml :'idropcopy/help/help', {:layout => :plainlayout}, :locals => {:link => @link}
+end
+
+
+get '/idropcopy/help/login/iphone' do
+  @link = '/idropcopy/help'
+  haml :'idropcopy/help/login/iphone', :locals => {:link => @link}
+end
+
+get '/idropcopy/p/help/login/iphone' do
+  @link = '/idropcopy/p/help'
+  haml :'idropcopy/help/login/iphone', {:layout => :plainlayout}, :locals => {:link => @link}
+end
+
 get '/style.css' do
   content_type 'text/css', :charset => 'utf-8'
   scss :style
